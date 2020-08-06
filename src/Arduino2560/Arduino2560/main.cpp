7ea582777f0f5240769a9fbd927642f33abb031b
dmitriy <dmitriy@radier.ca>
2020-08-06 15:39:34 -0400

    Converted from Arduino to Atmel Studio 7 project

---
+++ src/Arduino2560/Arduino2560/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Arduino2560.cpp
+ *
+ * Created: 8/4/2020 8:04:31 PM
+ * Author : Dmitriy
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
